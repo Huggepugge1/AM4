0fc3c80aafefa8cf1ff655a8b142a41e3f25c796
Hugo Lindstrom <huggepugge1@gmail.com>
2025-10-08 14:22:06 +0200

    docs: code_generation, lexer

--- assembler/src/code_generation.h
+++ assembler/src/code_generation.h
@@ -1,5 +1,11 @@
 #pragma once
 #include "parser.h"
 
+/**
+ * Generate am4 binary / machine code and write it to a file
+ *
+ * @param instructions InstructionVec of all instructions
+ * @param output_file File name of the output target
+ */
 void generate_code_and_write_to_file(struct InstructionVec *instructions,
                                      char *output_file);
--- assembler/src/lexer.h
+++ assembler/src/lexer.h
@@ -30,10 +30,40 @@ struct TokenVec {
     struct Token *elements;
 };
 
+/**
+ * Generate a vector (TokenVec) of tokens
+ *
+ * @param filename Name of the file to turn into tokens
+ *
+ * @returns All the tokens in a TokenVec
+ */
 struct TokenVec *lex(char *filename);
 
+/**
+ * Create a new TokenVec
+ *
+ * @returns empty TokenVec
+ */
 struct TokenVec *token_vec_new();
-void token_vec_push(struct TokenVec *vec, struct Token);
+
+/**
+ * Push a Token onto a TokenVec
+ *
+ * @param vec
+ * @param token
+ */
+void token_vec_push(struct TokenVec *vec, struct Token token);
+
+/**
+ * Destroy a TokenVec
+ *
+ * @param vec
+ */
 void token_vec_destroy(struct TokenVec *vec);
 
+/**
+ * Print all tokens in a TokenVec
+ *
+ * @param vec
+ */
 void token_vec_print(struct TokenVec *vec);
