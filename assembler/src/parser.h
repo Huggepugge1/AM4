05410bd43a72ca339b6aa9f457accb7fb680529d
Hugo Lindstrom <huggepugge1@gmail.com>
2025-10-08 15:08:25 +0200

    Fixed wrong opcodes for sub/mul

--- assembler/src/parser.h
+++ assembler/src/parser.h
@@ -9,8 +9,8 @@ enum InstructionKind {
     InstructionPush = 0x10,
 
     InstructionAdd = 0x20,
-    InstructionSub = 0x21,
-    InstructionMul = 0x22,
+    InstructionSub = 0x30,
+    InstructionMul = 0x40,
 
     InstructionNoop = 0x00,
 };
